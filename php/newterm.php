b5d2752fc3ad84e360853f7bcc11a5b148663231
Teraaret <raiiliiat@yandex.ru>
2017-10-31 16:51:43 +0500

    regesign
    - Rededigned hardly;
    - Now there are checking for such of title and text;

--- php/newterm.php
+++ php/newterm.php
@@ -1,8 +1,11 @@
 <?php
 
-    include "database.php";
-    $database = new DATABASE;
-    $data = $database->new_term( $_POST["title"], $_POST["text"] );
+    if ( !empty($_POST["title"]) && !empty($_POST["text"]) )
+    {
+        include "database.php";
+        $database = new DATABASE;
+        $data = $database->new_term( $_POST["title"], $_POST["text"] );
+    }
     header("location: /");
 
 ?>
