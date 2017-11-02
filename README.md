8e4df67d4498217242259968729a08c293c205e7
Teraaret <raiiliiat@yandex.ru>
2017-11-02 16:59:07 +0500

    clear for merging

--- README.md
+++
@@ -1,5 +0,0 @@
-# glossary-for-ms
-
-- Using php-adminer
-- Using vue.js
-- Created page vith input filter
--- php/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    include "database.php";
-    $database = new DATABASE;
-    $data = $database->del_term( $_GET["term"] );
-    header("location: /");
-
-?>
